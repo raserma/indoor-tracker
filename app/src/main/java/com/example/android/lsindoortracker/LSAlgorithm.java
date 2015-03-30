72afa3ddf327319a071c06552bcc46d12b64e73b
raserma <raserma91@gmail.com>
2015-03-30 18:33:16 +0200

    implementing weighted least squares, checking performance

--- app/src/main/java/com/example/android/lsindoortracker/LSAlgorithm.java
+++ app/src/main/java/com/example/android/lsindoortracker/LSAlgorithm.java
@@ -58,7 +58,7 @@ public class LSAlgorithm {
 
 
             /* Least Square algorithm */
-            Point userPosition = leastSquareAlgorithm (algorithmInputDataList);
+            Point userPosition = leastSquareAlgorithm(algorithmInputDataList);
             return userPosition;
         }
     }
@@ -273,7 +273,7 @@ public class LSAlgorithm {
         double b4_1 = (Math.pow(distAP1,2) - Math.pow(distAP4,2) + d4_1);
 
 
-        /* Generation of Matrix A and vector b */
+        /* Generation of Matrix A, Matrix W and vector b */
 
         double [][]matrixA = new double[][]{
 
@@ -292,18 +292,104 @@ public class LSAlgorithm {
 
 
         /* Linear Solver Least Square */
-        LinearSolver<DenseMatrix64F> solver = LinearSolverFactory.leastSquares(3, 3);
+        LinearSolver<DenseMatrix64F> solverL = LinearSolverFactory.leastSquares(3, 3);
 
-        if( !solver.setA(A) ) {
+        if( !solverL.setA(A) ) {
             throw new IllegalArgumentException("Singular matrix");
         }
 
-        if( solver.quality() <= 1e-8 )
+        if( solverL.quality() <= 1e-8 )
             throw new IllegalArgumentException("Nearly singular matrix");
 
-        solver.solve(b,x);
+        solverL.solve(b,x);
         double xUserPos = x.get(0) + coordAP1.x;
         double yUserPos = x.get(1) + coordAP1.y;
+
+        weightedLeastSquareAlgorithm(algInputList);
         return new Point((int)xUserPos, (int)yUserPos);
+    }
+    private Point weightedLeastSquareAlgorithm (List<APAlgorithmData> algInputList){
+        /* Gather all the collected data: AP coordinates and distances */
+
+        Point coordAP1 = algInputList.get(0).coordinatesAP;
+        Point coordAP2 = algInputList.get(1).coordinatesAP;
+        Point coordAP3 = algInputList.get(2).coordinatesAP;
+        Point coordAP4 = algInputList.get(3).coordinatesAP;
+
+        double distAP1 = algInputList.get(0).distance;
+        double distAP2 = algInputList.get(1).distance;
+        double distAP3 = algInputList.get(2).distance;
+        double distAP4 = algInputList.get(3).distance;
+
+        double d2_1 = Math.pow((coordAP2.x - coordAP1.x), 2) +  Math.pow((coordAP2.y - coordAP1
+                .y), 2);
+        double d3_1 = Math.pow((coordAP3.x - coordAP1.x), 2) +  Math.pow((coordAP3.y - coordAP1
+                .y), 2);
+        double d4_1 = Math.pow((coordAP4.x - coordAP1.x), 2) +  Math.pow((coordAP4.y - coordAP1
+                .y), 2);
+
+        double b2_1 = (Math.pow(distAP1,2) - Math.pow(distAP2,2) + d2_1);
+        double b3_1 = (Math.pow(distAP1,2) - Math.pow(distAP3,2) + d3_1);
+        double b4_1 = (Math.pow(distAP1,2) - Math.pow(distAP4,2) + d4_1);
+
+
+        /* Generation of Matrix A, Matrix W and vector b */
+
+        double [][]matrixA = new double[][]{
+
+                {coordAP2.x - coordAP1.x, coordAP2.y - coordAP1.y},
+                {coordAP3.x - coordAP1.x, coordAP3.y - coordAP1.y},
+                {coordAP4.x - coordAP1.x, coordAP4.y - coordAP1.y}};
+        DenseMatrix64F A = new DenseMatrix64F(matrixA);
+
+        double [][] matrixS = new double [][]{
+                {Math.pow(distAP1,4) + Math.pow(distAP2,4), Math.pow(distAP1,4),
+                        Math.pow(distAP1,4)},
+                {Math.pow(distAP1,4), Math.pow(distAP1,4) + Math.pow(distAP3,4),
+                        Math.pow(distAP1,4)},
+                {Math.pow(distAP1,4), Math.pow(distAP1,4), Math.pow(distAP1,
+                        4) +  Math.pow(distAP4,4)},
+        };
+
+        DenseMatrix64F S = new DenseMatrix64F(matrixS);
+
+        DenseMatrix64F SInv = new DenseMatrix64F(3,3);
+        CommonOps.invert(S, SInv);
+
+        double []vectorB = new double[]{
+                b2_1, b3_1, b4_1
+        };
+        DenseMatrix64F b = new DenseMatrix64F(3,1, false, vectorB);
+        CommonOps.scale(0.5,b); //1/2*b
+
+        DenseMatrix64F x = new DenseMatrix64F(2,1);
+
+
+        /* Linear Solver Least Square */
+
+        // A' = S^-1*A
+        DenseMatrix64F APrime = new DenseMatrix64F(3,2);
+        CommonOps.mult(SInv, A, APrime);
+
+        // b' = W*b
+        DenseMatrix64F bPrime = new DenseMatrix64F(3,1);
+        CommonOps.mult(SInv, b, bPrime);
+
+
+        // WEIGHTED LEAST SQUARE
+
+        LinearSolver<DenseMatrix64F> solverW = LinearSolverFactory.leastSquares(3, 3);
+
+        if( !solverW.setA(APrime) ) {
+            throw new IllegalArgumentException("Singular matrix");
+        }
+
+        if( solverW.quality() <= 1e-8 )
+            throw new IllegalArgumentException("Nearly singular matrix");
+
+        solverW.solve(bPrime,x);
+        double xUserPosW = x.get(0) + coordAP1.x;
+        double yUserPosW = x.get(1) + coordAP1.y;
+        return new Point((int)xUserPosW, (int)yUserPosW);
     }
 }
